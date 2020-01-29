83e47a7c0b05b7140a02d68f133c53baf31d2a78
Ryan <ryan.fee625@gmail.com>
2020-01-29 10:41:53 -0500

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,17 @@
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = var.vpc_name
+  cidr = var.cidr
+
+  azs             = ["us-east-1a", "us-east-1b"]
+  private_subnets = ["10.0.10.0/24", "10.0.11.0/24", "10.0.12.0/24"]
+
+  enable_nat_gateway = true
+  enable_vpn_gateway = true
+
+  tags = {
+    Terraform = "true"
+    Environment = "dev"
+  }
+}
